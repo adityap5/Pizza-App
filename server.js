65e4aa882a3076cca9f1ec10c762a4ab2e7bef85
adityap5 <aditya29pippal@gmail.com>
2024-04-06 18:10:43 +0530

    #3

--- server.js
+++ server.js
@@ -7,14 +7,23 @@ const expressLayout = require('express-ejs-layouts');
 
 app.use(express.static('public'));
 
+app.use(expressLayout)
+app.set('views',path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
     res.render('home')
 });
+app.get('/cart', (req, res)=>{
+    res.render('customers/cart')
+});
+app.get('/register', (req, res)=>{
+    res.render('auth/register')
+});
+app.get('/login', (req, res)=>{
+    res.render('auth/login')
+});
 
-app.use(expressLayout)
-app.set('views',path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs');
 
 
 app.listen(PORT,()=>{
